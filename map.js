4f5882fcbc52cc38474f814baad5c978c0fca86e
Sebastian Porling <sebastian.porling@gmail.com>
2020-03-18 11:21:01 +0100

    First task almost done

--- map.js
+++ map.js
@@ -2,6 +2,18 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 59.3498092, lng: 18.0684758},
-    zoom: 15
+	zoom: 25,
+	mapTypeId: 'satellite',
+	disableDefaultUI: true
   });
+}
+
+function tiltMap(){
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 59.329319, lng: 18.069632},
+		zoom: 25,
+		mapTypeId: 'satellite',
+		disableDefaultUI: true
+	  });
+	  map.setTilt(45);
 }
